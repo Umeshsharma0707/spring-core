62a43c5ab67df8214e23b8e4758c0266aa09fde4
umesh sharma <umeshsharma07072003@gmail.com>
2024-06-08 16:32:20 +0530

    injecting collections

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -1,7 +1,11 @@
 package com.springcore;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.springcore.collections.Employee;
 
 
 public class App 
@@ -11,7 +15,7 @@ public class App
         System.out.println( "Hello World! " ); 
         
     
-		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+  /*		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
         
         Student s1 = (Student) context.getBean("student1");
         
@@ -22,6 +26,23 @@ public class App
         
         Student s2 = (Student) context.getBean("student2");
         System.out.println(s2);
-        
+  */      
+		
+		
+	ApplicationContext ct = new ClassPathXmlApplicationContext("config.xml");	
+		Employee emp1 = (Employee) ct.getBean("employee1");
+		
+		System.out.println("name : " + emp1.getName());
+		System.out.println( "courses : " + emp1.getCourses());
+		System.out.println("phones : " + emp1.getPhones());
+		System.out.println("Addresses : " + emp1.getAddresses());
+		System.out.println("Employee props : " + emp1.getEmpProps());
+		
+		// traversing list of employee phones
+		List<String> empList = emp1.getPhones();
+		for(String e : empList) {
+			System.out.println(e);
+		}
+		
     }
 }
---
+++ src/main/java/com/springcore/collections/Employee.java
@@ -0,0 +1,67 @@
+package com.springcore.collections;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+public class Employee {
+	private String name;
+	private List<String> phones;
+	private Set<String> addresses;
+	private Map<String, String> courses;
+	private Properties empProps;
+	public Employee() {
+		super();
+		
+	}
+	public Employee(String name, List<String> phones, Set<String> addresses, Map<String, String> courses,
+			Properties empProps) {
+		super();
+		this.name = name;
+		this.phones = phones;
+		this.addresses = addresses;
+		this.courses = courses;
+		this.empProps = empProps;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<String> getPhones() {
+		return phones;
+	}
+	public void setPhones(List<String> phones) {
+		this.phones = phones;
+	}
+	public Set<String> getAddresses() {
+		return addresses;
+	}
+	public void setAddresses(Set<String> addresses) {
+		this.addresses = addresses;
+	}
+	public Map<String, String> getCourses() {
+		return courses;
+	}
+	public void setCourses(Map<String, String> courses) {
+		this.courses = courses;
+	}
+	public Properties getEmpProps() {
+		return empProps;
+	}
+	public void setEmpProps(Properties empProps) {
+		this.empProps = empProps;
+	}
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", phones=" + phones + ", addresses=" + addresses + ", courses=" + courses
+				+ ", empProps=" + empProps + "]";
+	}
+	
+	
+	
+	
+	
+}
