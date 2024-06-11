98e157d4d63916a5b05d4af215b6a33f640eff09
umesh sharma <umeshsharma07072003@gmail.com>
2024-06-11 13:18:47 +0530

    stereotype annotations

---
+++ src/main/java/com/springcore/stereotype/Student.java
@@ -0,0 +1,41 @@
+package com.springcore.stereotype;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	@Value("umesh sharma")
+	private String studentName;
+	@Value("Delhi")
+	private String city;
+	
+	private List<String> addresses;
+	
+	public List<String> getAddresses() {
+		return addresses;
+	}
+	public void setAddresses(List<String> addresses) {
+		this.addresses = addresses;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Student [studentName=" + studentName + ", city=" + city + ", addresses=" + addresses + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/springcore/stereotype/Test.java
@@ -0,0 +1,19 @@
+package com.springcore.stereotype;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		ApplicationContext context = 
+				new ClassPathXmlApplicationContext("com/springcore/stereotype/stereotype-config.xml");		
+		
+		Student s = context.getBean("student", Student.class);
+		
+		System.out.println(s);
+		s.setStudentName("umesh");
+		s.setCity("ahmedabad");
+		System.out.println(s);
+	}
+}
