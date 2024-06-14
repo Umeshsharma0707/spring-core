6a4415603126044f2b300eebdd6f37b6052c28a5
umesh sharma <umeshsharma07072003@gmail.com>
2024-06-14 22:13:13 +0530

    spring

--- src/main/java/com/springcore/practice/App.java
+++ src/main/java/com/springcore/practice/App.java
@@ -1,25 +1,25 @@
-//package com.springcore.practice;
-//
-//
-//import org.springframework.context.support.AbstractApplicationContext;
-//import org.springframework.context.support.ClassPathXmlApplicationContext;
-//
-//public class App {
-//	
-//	public static void main(String[] args) {
-//		
-//		AbstractApplicationContext context =
-//				new ClassPathXmlApplicationContext("com/springcore/practice/config.xml");
-//		
-//		
-//		Human h1 = context.getBean("human", Human.class);
-//		
-//		System.out.println(h1);
-//		
-//		context.registerShutdownHook();
-//		
-//		
-//		context.close();
-//	}
-//	
-//}
+package com.springcore.practice;
+
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		AbstractApplicationContext context =
+				new ClassPathXmlApplicationContext("com/springcore/practice/config.xml");
+		
+		
+		Human h1 = context.getBean("human", Human.class);
+		
+		System.out.println(h1);
+		
+		context.registerShutdownHook();
+		
+		
+		context.close();
+	}
+	
+}
