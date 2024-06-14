0d6540e03f0a94e9c1c26ff198c654331397c633
umesh sharma <umeshsharma07072003@gmail.com>
2024-06-14 22:15:07 +0530

    spring core practice

--- src/main/java/com/springcore/practice/Human.java
+++ src/main/java/com/springcore/practice/Human.java
@@ -3,8 +3,6 @@ package com.springcore.practice;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
 public class Human {
 	private String name;
